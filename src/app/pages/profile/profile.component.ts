06b672cdedf02b2bb26b277087d76df4abdeed75
Vandlik Viktor <h150209@stud.u-szeged.hu>
2025-04-06 20:54:32 +0200

    pipe

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -4,6 +4,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { UppercasePipe } from '../../shared/pipes/uppercase.pipe';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-profile',
@@ -12,10 +15,14 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatButtonModule,
     MatTableModule,
-    MatIconModule],
+    MatIconModule,
+    UppercasePipe,
+    CommonModule,
+    FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
+ 
 }
